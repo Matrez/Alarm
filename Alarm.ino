61165e21b6984ad18c2faaec2187abdfce9889be
Matrez <durifekete@gmail.com>
2019-03-04 21:12:02 +0100

    Refactor of pinModes and ifs

--- Alarm.ino
+++ Alarm.ino
@@ -1,26 +1,21 @@
 /*
-   2,3 cervena LED
-   4,5 zlta LED
-   6,7 zelena LED
-   8,9 tlacidla
-   10,11 senzory otvarania
-   12 senzor otrasu
-   13 bzuciak
-
+ * 2,3 cervena LED
+ * 4,5 zlta LED
+ * 6,7 zelena LED
+ * 8,9 tlacidla
+ * 10,11 senzory otvarania
+ * 12 senzor otrasu
+ * 13 bzuciak
+ *
 */
 
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, INPUT);
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-  pinMode(12, INPUT);
+  for (int i = 2; i <= 7; ++i) {
+    pinMode(i, OUTPUT);
+  }
+  for (int i = 8; i <= 12 ; ++i) {
+    pinMode(i, INPUT);
+  }
   pinMode(13, OUTPUT);
 }
 
@@ -33,30 +28,16 @@ void loop() {
 
 
   if (digitalRead(12) == LOW) {
-    digitalWrite(13, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    delay(500);
-    digitalWrite(13, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    delay(500);
-    digitalWrite(13, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    delay(500);
+    for (int i = 0; i < 3; ++i) {
+      digitalWrite(13, HIGH);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, HIGH);
+      delay(1000);
+      digitalWrite(13, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, LOW);
+      delay(500);
+    }
   };
 
 }
